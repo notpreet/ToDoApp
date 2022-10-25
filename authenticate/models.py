2a5fdc91c6bf28e3edfd2fddb1589e7f0483f144
notpreet <preet51199@gmail.com>
2022-10-25 16:50:18 -0400

    added models

--- authenticate/models.py
+++ authenticate/models.py
@@ -4,3 +4,16 @@ from django.db import models
 # Create your models here.
 
 
+class UserData(models.Model):
+    username = models.CharField(max_length=255, unique=True)
+    firstname = models.CharField(max_length=255)
+    lastname = models.CharField(max_length=255)
+    email = models.CharField(max_length=255)
+    profilepic = models.ImageField(blank=True, upload_to='images')
+
+
+class Tasks(models.Model):
+    task_categories = [('AC', 'Active'), ('CO', 'Completed')]
+    username = models.ForeignKey(UserData, on_delete=models.CASCADE)
+    task_name = models.CharField(max_length=255)
+    task_status = models.CharField(max_length=2, choices=task_categories, default='AC')
