d1d94ebfd5e5e7ed52c0fdc0a51b9250279e7d7a
notpreet <preet51199@gmail.com>
2022-10-25 16:53:09 -0400

    updated urls

--- authenticate/urls.py
+++ authenticate/urls.py
@@ -2,6 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.index, name='index'),
+    path('register', views.register, name='register'),
+    path('home', views.home, name='home'),
+    path('logout', views.logout_view, name='logout_view'),
+    path('createTask', views.createteask, name='createTask'),
     
 ]
 
